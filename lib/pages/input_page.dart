f72f8238a141c47a72f83ec372466cff261119c7
lucasliet <lucasouliveira@gmail.com>
2020-05-03 18:52:31 -0300

    bottom bar

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const _cardColor = Color(0xFF1D1E33);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -9,41 +11,50 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Calculadora IMC"),
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Card(color: Color(0xFF1D1E33)),
-                  ),
-                  Expanded(
-                    child: Card(color: Color(0xFF1D1E33)),
-                  ),
-                ],
-              ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Calculadora IMC"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Card(color: _cardColor),
+                ),
+                Expanded(
+                  child: Card(color: _cardColor),
+                ),
+              ],
             ),
-            Expanded(
-              child: Card(color: Color(0xFF1D1E33)),
+          ),
+          Expanded(
+            flex: 2,
+            child: Card(color: _cardColor),
+          ),
+          Expanded(
+            flex: 2,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Card(color: _cardColor),
+                ),
+                Expanded(
+                  child: Card(color: _cardColor),
+                ),
+              ],
             ),
-            Expanded(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Card(color: Color(0xFF1D1E33)),
-                  ),
-                  Expanded(
-                    child: Card(color: Color(0xFF1D1E33)),
-                  ),
-                ],
-              ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.pinkAccent,
             ),
-          ],
-        ));
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -57,10 +68,9 @@ class Card extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(12),
       decoration: BoxDecoration(
-          color: this.color,
-          borderRadius: BorderRadius.circular(12),
+        color: this.color,
+        borderRadius: BorderRadius.circular(12),
       ),
     );
   }
 }
-
