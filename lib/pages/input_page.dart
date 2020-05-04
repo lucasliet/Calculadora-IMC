937d7c6f11d807bbc3c274a6cd8cf09da2f26689
Lucas Souza <lucasouliveira@gmail.com>
2020-05-04 18:40:32 -0300

    altura sliders

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -6,10 +6,10 @@ import 'package:calculadora_imc/model/labeled_icon.dart';
 const _cardColor = Color(0xFF1D1E33);
 const _activeCardColor = Colors.green;
 const _cardTextStyle = TextStyle(
-    fontSize: 50,
-    fontWeight: FontWeight.w900,
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
 );
-enum Gender{
+enum Gender {
   male,
   female,
 }
@@ -21,7 +21,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender _gender;
-  int height = 180;
+  double _height = 1.65;
 
   @override
   Widget build(BuildContext context) {
@@ -44,8 +44,11 @@ class _InputPageState extends State<InputPage> {
                       children: <Widget>[
                         Expanded(
                           child: CustomCard(
-                            onPress: () => setState(() => _gender = Gender.male),
-                            color: _gender == Gender.male ? _activeCardColor : _cardColor,
+                            onPress: () =>
+                                setState(() => _gender = Gender.male),
+                            color: _gender == Gender.male
+                                ? _activeCardColor
+                                : _cardColor,
                             child: IconLabel(
                                 icon: FontAwesomeIcons.mars,
                                 label: 'MASCULINO'),
@@ -53,8 +56,11 @@ class _InputPageState extends State<InputPage> {
                         ),
                         Expanded(
                           child: CustomCard(
-                            onPress:  () => setState(() => _gender = Gender.female),
-                            color: _gender == Gender.female ? _activeCardColor : _cardColor,
+                            onPress: () =>
+                                setState(() => _gender = Gender.female),
+                            color: _gender == Gender.female
+                                ? _activeCardColor
+                                : _cardColor,
                             child: IconLabel(
                                 icon: FontAwesomeIcons.venus,
                                 label: 'FEMININO'),
@@ -65,26 +71,44 @@ class _InputPageState extends State<InputPage> {
                   ),
                   Expanded(
                     child: CustomCard(
-                      color: _cardColor,
-                      child: Column(
-                       mainAxisAlignment: MainAxisAlignment.center,
-                       children: <Widget>[
-                         Text('ALTURA', style: TextStyle(fontSize: 14)),
-                         Row(
-                           mainAxisAlignment: MainAxisAlignment.center,
-                           crossAxisAlignment: CrossAxisAlignment.baseline,
-                           textBaseline: TextBaseline.alphabetic,
-                           children: <Widget>[
-                             Text(
-                               height.toString(),
-                               style: _cardTextStyle,
-                             ),
-                             Text('cm '),
-                           ],
-                         )
-                       ], 
-                      ) 
-                    ),
+                        color: _cardColor,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text('ALTURA', style: TextStyle(fontSize: 14)),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.baseline,
+                              textBaseline: TextBaseline.alphabetic,
+                              children: <Widget>[
+                                Text(
+                                  _height.toStringAsFixed(2),
+                                  style: _cardTextStyle,
+                                ),
+                                Text('m'),
+                              ],
+                            ),
+                            SliderTheme(
+                              data: SliderTheme.of(context).copyWith(
+                                thumbShape: RoundSliderThumbShape(
+                                    enabledThumbRadius: 15.0),
+                                overlayShape: RoundSliderOverlayShape(
+                                    overlayRadius: 30.0),
+                                thumbColor: Colors.cyanAccent,
+                                overlayColor: Color(0x2984FFFF),
+                                activeTrackColor: Colors.lightBlue[100],
+                                inactiveTrackColor: Colors.blueGrey,
+                              ),
+                              child: Slider(
+                                value: _height,
+                                min: 1.0,
+                                max: 3.0,
+                                onChanged: (double _newHeight) =>
+                                    setState(() => _height = _newHeight),
+                              ),
+                            )
+                          ],
+                        )),
                   ),
                   Expanded(
                     child: Row(
@@ -105,9 +129,9 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: Container(
               decoration: BoxDecoration(
-                  color: Colors.pink,
-                  borderRadius:
-                      BorderRadius.vertical(top: Radius.circular(12))),
+                color: Color(0xFFEB1555),
+                borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
+              ),
             ),
           )
         ],
