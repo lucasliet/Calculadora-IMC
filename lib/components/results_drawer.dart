8f19a0c46e2783f079830590e857c71c0c0f16f0
Lucas Souza <lucasouliveira@gmail.com>
2020-06-24 18:41:13 -0300

    Results showing on drawer

--- lib/components/results_drawer.dart
+++ lib/components/results_drawer.dart
@@ -4,7 +4,7 @@ import 'package:calculadora_imc/model/services/result_service.dart';
 import 'package:calculadora_imc/theme.dart';
 import 'package:flutter/material.dart';
 
-class ResultsDrawer extends StatefulWidget{
+class ResultsDrawer extends StatefulWidget {
   @override
   _ResultsDrawerState createState() => _ResultsDrawerState();
 }
@@ -12,13 +12,17 @@ class ResultsDrawer extends StatefulWidget{
 class _ResultsDrawerState extends State<ResultsDrawer> {
   List<Result> results = [];
 
+  void _getResults() async {
+    ResultService service = new ResultService();
+    List<Result> response = await service.getResults();
+    setState(() => results = response);
+    print(response);
+  }
+
   @override
   void initState() {
     super.initState();
-    ResultService service = new ResultService();
-    service.getResults().then( (List<Result> response) {
-       setState( () => results.addAll(response) );
-    });
+    _getResults();
   }
 
   @override
@@ -35,37 +39,45 @@ class _ResultsDrawerState extends State<ResultsDrawer> {
                 child: Text('Seus resultados', style: titleStyle),
               ),
             ),
-            ListView.builder(
-              itemBuilder: (context, index) {
-                return CustomCard(
-                  child: ListTile(
-                    contentPadding: EdgeInsets.symmetric(horizontal: 12),
-                    title: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text('Data'),
-                            Text(results[index].date , style: TextStyle(fontSize: 16, fontWeight: FontWeight.w800)),
-                          ],
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: <Widget>[
-                            Text('Resultado'),
-                            Text(results[index].result.toString(), style: TextStyle(fontSize: 16, fontWeight: FontWeight.w800)),
-                          ],
-                        ),
-                      ],
+            Container(
+              height: double.maxFinite,
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return CustomCard(
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text('Data'),
+                              Text(results[index].date,
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w800)),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: <Widget>[
+                              Text('Resultado'),
+                              Text(results[index].result.toString(),
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w800)),
+                            ],
+                          ),
+                        ],
+                      ),
+                      onTap: () {},
                     ),
-                    onTap: () {},
-                  ),
-                );
-              } ,
-              itemCount: results.length,
+                  );
+                },
+                itemCount: results.length,
+              ),
             )
-
           ],
         ),
       ),
